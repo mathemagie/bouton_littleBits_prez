4fbab084c57bb52b67902bb52e074926fab28775
Fache Aurélien <aurelien.fache@gmail.com>
2016-02-29 14:55:57 +0100

    main

---
+++ index.py
@@ -0,0 +1,18 @@
+from serial import Serial
+from StringIO import StringIO
+from os import system
+
+def touche_next():
+	system('osascript -e \'tell application "System Events" to key code 124\'')
+
+def touche_prev():
+	system('osascript -e \'tell application "System Events" to key code 123\'')
+
+serial_port = Serial(port='/dev/tty.usbmodem1411', baudrate=57600)
+
+while True:
+	#serial_port.write('100'.encode('ascii'))
+	str = serial_port.readline()
+	print str
+	if 'on' in str:
+		touche_next()
